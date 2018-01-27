7cadddc4034343806e27544b35733e8f6851bda1
Bogdan Arabadzhi <bogdan.today@gmail.com>
2018-01-27 01:52:09 +0100

    Refactor the project

--- src/knapsack/mod.rs
+++ src/knapsack/mod.rs
@@ -3,13 +3,15 @@ extern crate colored;
 pub mod item;
 
 use std::fmt;
+use std::fs::File;
 use std::time::Duration;
+use std::io::{BufRead, BufReader};
 
 use self::colored::*;
 
 use self::item::Item;
 
-#[derive(Default, Clone)]
+#[derive(Clone, Default)]
 pub struct Knapsack {
     pub n: usize,
     pub m: usize,
@@ -19,17 +21,85 @@ pub struct Knapsack {
     pub statistics: Statistics,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Clone, Default, Debug)]
 pub struct Statistics {
     pub total_profit: u32,
     pub duration: Duration,
 }
 
 impl Knapsack {
-    pub fn new() -> Knapsack {
-        Knapsack {
+    fn new() -> Self {
+        Self {
             ..Default::default()
         }
+    }
+
+    pub fn from(file: &str) -> Self {
+        let file = File::open(file).expect("Input file is not specified");
+        let reader = BufReader::new(file);
+
+        let mut m = 0;
+        let mut profits = Vec::new();
+        let mut weights = Vec::new();
+
+        let mut knapsack = Knapsack::new();
+
+        for (line_number, contents) in reader.lines().enumerate() {
+            let mut contents: Vec<u16> = contents
+                .unwrap()
+                .split_whitespace()
+                .map(|x| x.parse::<u16>().unwrap())
+                .collect();
+
+            match line_number {
+                0 => {
+                    // n m q opt
+                    debug_assert_eq!(contents.len(), 4);
+
+                    knapsack.n = contents[0] as usize;
+                    knapsack.m = contents[1] as usize;
+                    knapsack.items = Vec::with_capacity(knapsack.m);
+
+                    m = knapsack.m + 1;
+                }
+                1 => {
+                    // a line with the n obj. func. coefficients
+                    debug_assert_eq!(contents.len(), knapsack.n);
+                    profits = contents;
+                }
+                _ if m >= line_number => {
+                    // a line for each m; n coefficients for <= constraints
+                    weights.push(contents.into_boxed_slice());
+                }
+                _ => {
+                    // a line with rhs of <= constraints
+                    debug_assert_eq!(contents.len(), knapsack.m);
+                    knapsack.capacity = contents.into_boxed_slice();
+                }
+            }
+        }
+
+        for (index, profit) in profits.into_iter().enumerate() {
+            let mut item_weights = Vec::with_capacity(weights.len());
+
+            for weight in &weights {
+                item_weights.push(weight[index]);
+            }
+
+            knapsack.items.push(Item {
+                profit: profit,
+                weights: item_weights.into_boxed_slice(),
+                used: false,
+                id: index + 1,
+            });
+        }
+
+        knapsack
+    }
+
+    pub fn heuristic(mut self, name: &str) -> Self {
+        self.heuristic = name.into();
+        self
     }
 }
 
@@ -41,7 +111,7 @@ impl fmt::Debug for Knapsack {
             items += &format!("\t{:?}\n", item);
         }
 
-        items.pop();
+        items.trim_right_matches('\n');
 
         write!(
             f,
@@ -71,19 +141,18 @@ impl fmt::Display for Knapsack {
 
         items.sort();
 
-        let mut item_list = String::new();
+        let mut list_of_items = String::new();
 
         for item in &items {
-            item_list += &format!("{:?}, ", item);
+            list_of_items += &format!("{:?}, ", item);
         }
 
-        item_list.pop();
-        item_list.pop();
+        list_of_items.trim_right_matches(", ");
 
-        let mut time = String::new();
+        let mut run_time = String::new();
 
         if self.statistics.duration.as_secs() > 0 {
-            time += &format!(
+            run_time += &format!(
                 "{} {}",
                 self.statistics.duration.as_secs().to_string().green(),
                 's'
@@ -91,14 +160,14 @@ impl fmt::Display for Knapsack {
         }
 
         if self.statistics.duration.subsec_nanos() > 0 {
-            if time.is_empty() {
-                time += &format!(
+            if run_time.is_empty() {
+                run_time += &format!(
                     "{} {}",
                     self.statistics.duration.subsec_nanos().to_string().green(),
                     "ns"
                 )
             } else {
-                time += &format!(
+                run_time += &format!(
                     " {} {}",
                     self.statistics.duration.subsec_nanos().to_string().green(),
                     "ns"
@@ -116,8 +185,8 @@ impl fmt::Display for Knapsack {
             self.heuristic.cyan().bold(),
             self.statistics.total_profit.to_string().green(),
             items.len(),
-            item_list.yellow(),
-            time
+            list_of_items.yellow(),
+            run_time
         )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ extern crate rayon;
 
 mod knapsack;
 
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
 use clap::{App, Arg};
@@ -13,108 +11,20 @@ use rand::{thread_rng, Rng};
 use rayon::prelude::*;
 
 use knapsack::Knapsack;
-use knapsack::item::Item;
 
 fn main() {
-    let matches = App::new("MKP")
-        .version("0.1.0")
-        .author("Bogdan Arabadzhi <bogdan.today@gmail.com>")
-        .about("Construction heuristics for the multidimensional knapsack problem (MKP)")
-        .arg(
-            Arg::with_name("input")
-                .short("i")
-                .long("input")
-                .value_name("FILE")
-                .help("Sets a custom input file")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("random")
-                .short("r")
-                .long("random")
-                .value_name("NUMBER")
-                .help("Sets a number of random heuristic iterations")
-                .takes_value(true),
-        )
-        .get_matches();
+    let (file, greedy_runs) = read_cmd_arguments();
 
-    let input = matches.value_of("input").unwrap_or("input.txt");
-    let n = matches
-        .value_of("random")
-        .unwrap_or("10")
-        .parse::<usize>()
-        .unwrap();
+    let knapsack = Knapsack::from(&file).heuristic("Random");
+    let mut knapsacks = Vec::with_capacity(greedy_runs);
 
-    let mut knapsack = Knapsack::new();
-
-    let file = File::open(input).expect("Input file is not specified");
-    let reader = BufReader::new(file);
-
-    let mut m = 0;
-    let mut profits = Vec::new();
-    let mut weights = Vec::new();
-
-    for (number, contents) in reader.lines().enumerate() {
-        let mut contents: Vec<u16> = contents
-            .unwrap()
-            .split_whitespace()
-            .map(|x| x.parse::<u16>().unwrap())
-            .collect();
-
-        match number {
-            0 => {
-                // n m q opt
-                debug_assert_eq!(contents.len(), 4);
-
-                knapsack.n = contents[0] as usize;
-                knapsack.m = contents[1] as usize;
-                knapsack.items = Vec::with_capacity(knapsack.m);
-
-                m = knapsack.m + 1;
-            }
-            1 => {
-                // a line with the n obj. func. coefficients
-                debug_assert_eq!(contents.len(), knapsack.n);
-                profits = contents;
-            }
-            _ if number <= m => {
-                // a line for each m; n coefficients for <= constraints
-                weights.push(contents.into_boxed_slice());
-            }
-            _ => {
-                // a line with rhs of <= constraints
-                debug_assert_eq!(contents.len(), knapsack.m);
-                knapsack.capacity = contents.into_boxed_slice();
-            }
-        }
-    }
-
-    for (index, profit) in profits.into_iter().enumerate() {
-        let mut w = Vec::with_capacity(weights.len());
-
-        for weight in &weights {
-            w.push(weight[index]);
-        }
-
-        knapsack.items.push(Item {
-            profit: profit,
-            weights: w.into_boxed_slice(),
-            used: false,
-            id: index + 1,
-        });
-    }
-
-    knapsack.heuristic = String::from("Random");
-
-    let mut knapsacks = Vec::with_capacity(n);
-
-    for _ in 0..n {
+    for _ in 0..greedy_runs {
         let mut k = knapsack.clone();
         thread_rng().shuffle(&mut k.items);
         knapsacks.push(k);
     }
 
-    debug_assert_eq!(knapsacks.len(), n);
+    debug_assert_eq!(knapsacks.len(), greedy_runs);
 
     let time = Instant::now();
 
@@ -147,38 +57,71 @@ fn main() {
         knapsack.statistics.duration = elapsed;
     }
 
-    knapsack.heuristic = String::from("Greedy");
+    // knapsack.heuristic("Greedy");
 
-    let time_2 = Instant::now();
+    // let time_2 = Instant::now();
 
-    knapsack.items.sort_unstable_by(|a, b| b.cmp(a));
+    // knapsack.items.sort_unstable_by(|a, b| b.cmp(a));
 
-    let mut item_can_be_used = false;
+    // let mut item_can_be_used = false;
 
-    for item in &mut knapsack.items {
-        for (index, constraint) in knapsack.capacity.iter().enumerate() {
-            if item.weights[index] > *constraint {
-                item_can_be_used = false;
-                break;
-            } else {
-                item_can_be_used = true;
-            }
-        }
+    // for item in &mut knapsack.items {
+    //     for (index, constraint) in knapsack.capacity.iter().enumerate() {
+    //         if item.weights[index] > *constraint {
+    //             item_can_be_used = false;
+    //             break;
+    //         } else {
+    //             item_can_be_used = true;
+    //         }
+    //     }
 
-        if item_can_be_used {
-            for (index, constraint) in knapsack.capacity.iter_mut().enumerate() {
-                *constraint -= item.weights[index];
-                item.used = true;
-                knapsack.statistics.total_profit += u32::from(item.profit);
-            }
-        }
-    }
+    //     if item_can_be_used {
+    //         for (index, constraint) in knapsack.capacity.iter_mut().enumerate() {
+    //             *constraint -= item.weights[index];
+    //             item.used = true;
+    //             knapsack.statistics.total_profit += u32::from(item.profit);
+    //         }
+    //     }
+    // }
 
-    knapsack.statistics.duration = time_2.elapsed();
+    // knapsack.statistics.duration = time_2.elapsed();
 
     println!("{}", knapsack);
 
     for item in knapsacks {
         println!("{}", item);
     }
+}
+
+fn read_cmd_arguments() -> (String, usize) {
+    let matches = App::new("MKP")
+        .version("0.1.0")
+        .author("Bogdan Arabadzhi <bogdan.today@gmail.com>")
+        .about("Construction heuristics for the multidimensional knapsack problem (MKP)")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .help("Sets a custom input file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("random")
+                .short("r")
+                .long("random")
+                .value_name("NUMBER")
+                .help("Sets a number of random heuristic iterations")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let input = matches.value_of("input").unwrap_or("input.txt").into();
+    let random = matches
+        .value_of("random")
+        .unwrap_or("10")
+        .parse::<usize>()
+        .unwrap();
+
+    (input, random)
 }
